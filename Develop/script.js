2f46d19d99030b7d37212c11b126d7e98ea756b8
rob <collinsduo1@gmail.com>
2023-06-02 18:34:49 -0500

    add style and elements

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
-//https://www.w3schools.com/jquery/
+// website that I used to help me complete this//
+//https://www.w3schools.com/jquery//
+///https://momentjs.com///Time and Date//
 $(document).ready(function(){
-
-  ///https://momentjs.com///Time and Date//
-  //https://www.youtube.com/watch?v=LvIt1GuS_DI
+  
   //Hours and military times array//
   var today = moment().format("dddd, MMMM Do");
   $("#currentDay").text(today);
@@ -20,3 +20,22 @@ $(document).ready(function(){
           events.addClass("future");
       };
   };
+
+  //Hours and military times array//
+  //https://www.tutorialsteacher.com/javascript/javascript-for-loop//
+  for (var i = 0; i < hour.length; i++) {
+    //Create html elements, jQuery//
+    var newBtn = $("<btn>");
+    var newDiv = $("<div>");
+    var newInput = $("<input>");
+    var newP = $("<p>");
+   
+
+ //Add styles //
+ newBtn.addClass("col-sm-1 saveBtn fa fa-save");
+ newDiv.addClass("row");
+ newInput.addClass("col-sm-10 description");
+ newInput.attr("id", militaryTimes[i]);
+ newP.addClass("col-sm-1 time-block hour");
+ newP.text(hour[i]);
+
